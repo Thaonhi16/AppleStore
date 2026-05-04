8116981c58273bc70eb102d943a19b10da62663d
Thaonhi16 <tothaonhi2004@gmail.com>
2026-05-05 01:12:58 +0700

    feat(frontend): init React application

---
+++ fe/src/pages/home.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import Banner from "../components/home/banner";
+import Product from "../components/home/product";
+
+
+
+const HomePage = () => {
+
+
+
+    return (
+        <>
+            <Banner />
+            <Product />
+
+        </>
+    );
+
+
+}
+
+export default HomePage;    
